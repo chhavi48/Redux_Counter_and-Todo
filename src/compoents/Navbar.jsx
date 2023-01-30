b5fc8942d6e0e10dd76d0fd01c39940cd632288e
Chhavi srivastav <77965216+chhavi48@users.noreply.github.com>
2023-01-30 22:10:14 +0530

    setup mui

--- src/compoents/Navbar.jsx
+++ src/compoents/Navbar.jsx
@@ -1,18 +1,57 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+// import MenuIcon from '@mui/icons-material/Menu';
+
 const Navbar = () => {
   return (
-    <div style={{
-        padding:"30px",
-        display:"flex",
-        gap:"20px",
-        fontSize:"20px",
-        border:"1px solid black"
+    // <div style={{
+    //     padding:"30px",
+    //     display:"flex",
+    //     gap:"20px",
+    //     fontSize:"20px",
+    //     border:"1px solid black"
        
-    }}>
+    // }}>
+    //     
+    //   
+    // </div>
+
+    <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="white"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          {/* <MenuIcon /> */}
+        </IconButton>
+        <Typography variant="h6" 
+        component="div"sx={{ flexGrow: 1 }}>
         <Link to="/">Counter</Link>
-        <Link to="/TodoApp">TodoApp</Link>
-    </div>
+        
+      
+        </Typography>
+        <Typography variant="h6" 
+        color='white'
+        component="div"sx={{ flexGrow: 1 }}>
+  
+        <Link to="/TodoApp" underline='none'>Todo</Link>
+      
+        </Typography>
+
+      </Toolbar>
+    </AppBar>
+  </Box>
+
   )
 }
 
